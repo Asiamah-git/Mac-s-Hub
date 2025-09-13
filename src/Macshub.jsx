e71d81fc546f15aa9c7eae6b20ec319c34197f54
Asiamah-git <85683300+Asiamah-git@users.noreply.github.com>
2025-09-13 06:49:23 +0000

    try1

--- src/Macshub.jsx
+++ src/Macshub.jsx
@@ -234,7 +234,7 @@ function Promotions() {
 
   const flyers = [
     {
-      src: "../flyer1buscert.png",
+      src: "./src/flyer1buscert.png",
       desc: "Exclusive Startup Growth Workshop — Join us this weekend!",
     },
     {
